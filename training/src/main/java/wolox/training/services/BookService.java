18696998d2d3d6aeda17b85ca239660b86dcdec7
Lucas Miotti <lmiotti@MacBook-Pro-de-wolox.local>
2020-02-14 17:16:21 -0300

    Changed log.debug() to lo log.info()

--- training/src/main/java/wolox/training/services/BookService.java
+++ training/src/main/java/wolox/training/services/BookService.java
@@ -17,35 +17,35 @@ public class BookService {
     private BookRepository bookRepository;
 
     public Book createBook(Book book) {
-        log.debug(String.format("%s was received to POST", book.toString()));
+        log.info(String.format("%s was received to POST", book.toString()));
         return bookRepository.save(book);
     }
 
     public Book readBook(long id) {
-        log.debug(String.format("%d was received to search Book",id));
+        log.info(String.format("%d was received to search Book",id));
         return bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
     }
 
     public Book findByAuthor(String author) {
-        log.debug(String.format("%s (author) was received to search for Book",author));
+        log.info(String.format("%s (author) was received to search for Book",author));
         return bookRepository.findFirstByAuthor(author).orElseThrow(BookAuthorMismatchException::new);
     }
 
     public Book updateBook(Book book, long id) {
-        log.debug(String.format("%s (book) and %d (id) was received to look for Book",book,id));
+        log.info(String.format("%s (book) and %d (id) was received to look for Book",book,id));
         if (book.getId() != id) {
             log.error("Book's id mismatches id");
             throw new BookIdMismatchException();
         }
         readBook(id);
-        log.debug("Book was found");
+        log.info("Book was found");
         return bookRepository.save(book);
     }
 
     public void deleteBook(long id) {
-        log.debug(String.format("%d was found to delete book",id));
+        log.info(String.format("%d was found to delete book",id));
         readBook(id);
-        log.debug("Book was found");
+        log.info("Book was found");
         bookRepository.deleteById(id);
     }
 }
